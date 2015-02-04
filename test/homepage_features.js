4ebf261bc6b241d65c7e5f36e9874414d77adec5
Andy <andygnewman@gmail.com>
2015-02-04 13:03:55 +0000

    add first test issues encountered

---
+++ test/homepage_features.js
@@ -0,0 +1,13 @@
+describe('homepage', function() {
+
+  before(function(){
+    casper.start('http://localhost:9999/');
+  });
+
+  it('should say hello world', function(){
+    casper.then(function() {
+      expect('body').to.have.text('Hello World');
+    });
+  });
+
+});
