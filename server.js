1fa1eb6d174983dadb3e5493e282cc1f2fba4fb8
Andy <andygnewman@gmail.com>
2015-02-04 17:11:52 +0000

    create new end points including query parameters

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+app.set('view engine', 'ejs');
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World');
+  response.render('index', request.query);
 });
 
 app.get('/beverages', function(request, response) {
-  response.send('Tea');
+  response.json({"Tea":"Tetley"});
 });
 
 server.listen(9999, function() {
--- test/homepage_features.js
+++ test/homepage_features.js
@@ -1,12 +1,26 @@
 describe('homepage', function() {
 
+var host = 'http://localhost:9999/';
+
   before(function(){
-    casper.start('http://localhost:9999/');
+    casper.start(host);
   });
 
   it('should say hello world', function(){
     casper.then(function() {
       expect('body').to.have.text('Hello World');
+    });
+  });
+
+  it('should say hello Steph!', function() {
+    casper.thenOpen(host + '?name=Steph', function() {
+      expect('body').to.have.text('Hello Steph');
+    });
+  });
+
+  it('should return a JSON object', function() {
+    casper.thenOpen(host + 'beverages', function() {
+      expect('body').to.have.text('{"Tea":"Tetley"}');
     });
   });
 
